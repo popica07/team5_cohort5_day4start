0b56cff1265b28b2315099424fb3075dde35a55d
victor13245 <victormanoilescu@gmail.com>
2026-07-27 15:53:45 +0300

    ticket 56 completed

--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-27T15:34:36+0300",
+    date = "2026-07-27T15:51:28+0300",
     comments = "version: 1.6.2, compiler: javac, environment: Java 26.0.2 (Oracle Corporation)"
 )
 @Component
