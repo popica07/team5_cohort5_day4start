a42c4132312e1527b7f545179a587791cbc6e4ff
kiruthiharan <kiruthiharan@gmail.com>
2026-08-04 10:33:41 +0200

    fix frontend apis to fetch from backend

--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-08-04T09:57:35+0200",
+    date = "2026-08-04T10:14:06+0200",
     comments = "version: 1.6.2, compiler: javac, environment: Java 25-loom (Oracle Corporation)"
 )
 @Component
